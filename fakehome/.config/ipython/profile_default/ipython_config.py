dc2d3c7eed5a100131cbcbe7385dfbf11a66e1ec
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2021-01-03 20:24:31 +0600

    Add ipython config

---
+++ fakehome/.config/ipython/profile_default/ipython_config.py
@@ -0,0 +1,4 @@
+c.InteractiveShell.colors = 'linux'
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.highlight_matching_brackets = True
+c.TerminalInteractiveShell.confirm_exit = False
