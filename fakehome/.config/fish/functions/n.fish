dea2427ca6f73971331bd3786347688bbdc9aa50
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2022-05-31 17:28:12 -0400

    Add btop, userChrome, touchpad & env conf

--- fakehome/.config/fish/functions/n.fish
+++ fakehome/.config/fish/functions/n.fish
@@ -20,7 +20,7 @@ function n --wraps nnn --description 'support nnn quit and change directory'
     set -x NNN_COLORS "4123"
     set -x NNN_TRASH 1
     set -x NNN_OPENER "$HOME/.config/nnn/plugins/nuke"
-    set -x NNN_BMS "c:~/Documents/Coding/;w:~/Pictures/WallE/;b:~/Documents/Books"
+    set -x NNN_BMS "c:~/Documents/Coding;w:~/Pictures/WallE;b:~/Documents/Books"
     set -x NNN_PLUG "f:preview-tabbed"
 
     nnn -acdHT v $argv
