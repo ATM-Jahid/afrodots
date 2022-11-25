0b40b88fb6e1a3cf16ae1ff21e6168073e61420d
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2022-11-25 15:23:06 -0500

    Streamline swww_randomize

--- fakehome/.config/waybg/swaybg_randomize.sh
+++ fakehome/.config/waybg/swaybg_randomize.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-swaybg -i $(find ~/Pictures/WallE/ -type f | shuf -n 1) -m fill &
+swaybg -i $(find ~/Pictures/WallE -type f | shuf -n 1) -m fill &
 OLD_PID=$!
 
 while true; do
 	sleep 55
-	swaybg -i $(find ~/Pictures/WallE/ -type f | shuf -n 1) -m fill &
+	swaybg -i $(find ~/Pictures/WallE -type f | shuf -n 1) -m fill &
 	NEXT_PID=$!
 	sleep 5
 	kill $OLD_PID
--- fakehome/.config/waybg/swww_randomize.sh
+++ fakehome/.config/waybg/swww_randomize.sh
@@ -1,34 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-# This script will randomly go through the files of a directory, setting it
-# up as the wallpaper at regular intervals
-#
-# NOTE: this script is in bash (not posix shell), because the RANDOM variable
-# we use is not defined in posix
-
-if [[ $# -lt 1 ]] || [[ ! -d $1   ]]; then
-	echo "Usage:
-	$0 <dir containing images>"
-	exit 1
-fi
-
-# Edit bellow to control the images transition
+# Edit below to control the images transition
 export SWWW_TRANSITION_SPEED=10
 export SWWW_TRANSITION="random"
-
-# This controls (in seconds) when to switch to the next image
-INTERVAL=60
 
 swww init
 
 while true; do
-	find "$1" \
-		| while read -r img; do
-			echo "$((RANDOM % 1000)):$img"
-		done \
-		| sort -n | cut -d':' -f2- \
-		| while read -r img; do
-			swww img "$img"
-			sleep $INTERVAL
-		done
+	swww img $(find ~/Pictures/WallE -type f | shuf -n 1)
+	sleep 60
 done
