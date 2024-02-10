9d097ab4cc0b05d5ba80d22055eb08a5d8b824db
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2024-02-09 20:32:13 -0500

    Add wezterm config

---
+++ fakehome/.config/wezterm/wezterm.lua
@@ -0,0 +1,39 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+--config.color_scheme = "tokyonight_storm"
+
+config.keys = {
+	{
+		key = "Enter",
+		mods = "ALT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
+
+config.color_scheme = "Solarized Dark Higher Contrast"
+config.font_size = 9
+
+config.window_background_opacity = 0.8
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 3,
+	right = 2,
+	top = 0,
+	bottom = 0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
