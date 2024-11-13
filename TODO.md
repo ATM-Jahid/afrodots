6ac5bfb9b5855a530502d316caed6286198a829c
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2024-11-12 22:52:13 -0500

    Modify nuke, nvim, waybar & pkglist
    - Open ppm files with sxiv
    - Make "//" the commentstring for C/C++
    - Disable friendly-snippets again
    - Fix the firefox icon
    - Switch to fastfetch

--- TODO.md
+++ TODO.md
@@ -1,4 +1,8 @@
 # TODO
 Things that I need to fix/implement.
 
+- Use swww
+- Move to ags
+- Update pkglist
 - Fix pipewire mic issue
+- Do not move to hypr ecosystem (super buggy)
--- fakehome/.config/nvim/lua/plugins/disable.lua
+++ fakehome/.config/nvim/lua/plugins/disable.lua
@@ -4,4 +4,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
 }
